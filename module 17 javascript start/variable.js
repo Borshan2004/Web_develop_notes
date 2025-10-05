16e6955e351cc3b50f91309bb1cf8d7a1a893dfa
Borshan2004 <borshanpurification42@gmail.com>
2025-10-05 23:38:21 +0600

    this is

---
+++ module 17 javascript start/variable.js
@@ -0,0 +1,14 @@
+var age = 38 ;
+var weight = 120 ;
+var height = 23;
+
+// console.log(height);
+
+// var time = 12;
+// console.log(time);
+
+var money = 20.10;
+console.log(money);
+
+var hasan;
+console.log(hasan);
