3b5e04562d11f4b1731cb39c3d3e920e057d1128
Borshan2004 <borshanpurification42@gmail.com>
2025-10-07 00:27:41 +0600

    javascript second

---
+++ module 18.5 array in javascript/PushandPop.js
@@ -0,0 +1,37 @@
+// push add in the last of the array
+const array = [10,20,30];
+console.log(array);
+
+array.push(10);
+
+console.log(array);
+
+array.push(10,156,45);
+
+console.log(array);
+
+
+const str= ['hasn','rahim','kamal'];
+console.log(str);
+
+str.push("kamlasdfsd");
+console.log(str);
+
+//pop remove from the last of the array
+
+const popItem= str.pop();
+console.log(str);
+console.log(popItem);
+
+
+//shift is used to remove the first form the the string 
+const str2=['boss','loss','hoss','noss'];
+console.log(str2);
+
+str2.shift();
+console.log(str2);
+
+
+//unshift is used to add in the first 
+str2.unshift("borshan Purification");
+console.log(str2);
---
+++ module 18.5 array in javascript/array.js
@@ -0,0 +1,15 @@
+const element =[10,20,30,40,50];
+console.log(element.length);
+
+const element2 = [20,30,40,50,60,70,80,90,100];
+console.log(element2.length);
+
+console.log(element2);
+
+console.log(element2[2]);
+
+const element4 = element2[3];
+console.log(element4);
+
+element2[4]=1054;
+console.log(element2);
