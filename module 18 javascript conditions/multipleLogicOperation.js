779ab40150010fc579965a0db806d0974f25d4cd
Borshan2004 <borshanpurification42@gmail.com>
2025-10-06 19:57:17 +0600

    condition 2

---
+++ module 18 javascript conditions/multipleLogicOperation.js
@@ -0,0 +1,28 @@
+//when you use the const you can not change the   variable it is 
+
+const BcsCader =true;
+const Salary = 20000;
+const height = 76;
+
+// if(BcsCader==true||Salary>25000||height>79){
+//     console.log("Not a bad choice");
+// }
+// else{
+//     console.log("its a very bad choice");
+// }
+
+
+
+// if(BcsCader==true&&Salary>25000&&height>79){
+//     console.log("perfect  choice");
+// }
+// else{
+//     console.log("its a very bad choice");
+// }
+
+if((Salary>18000&&height>70)||BcsCader==true){
+    console.log("good for this");
+}
+else{
+    console.log("It is a very much bad choice");
+}
