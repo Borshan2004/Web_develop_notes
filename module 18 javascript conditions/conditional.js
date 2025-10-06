9f1934bf942f306986ab0b8d21b1b22836f8618a
Borshan2004 <borshanpurification42@gmail.com>
2025-10-06 10:47:58 +0600

    fdas

---
+++ module 18 javascript conditions/conditional.js
@@ -0,0 +1,15 @@
+// console.log(10>5);
+// console.log(5>10);
+// console.log(5>=2);
+// console.log(6<=6);
+// console.log(100<=10);
+
+// var borsahn_marks=50;
+// var HasanMark =20;
+// console.log(borsahn_marks>HasanMark);
+
+//only check the value 
+console.log(10=='10');
+
+//check the value and also the type
+console.log(10==='10');
