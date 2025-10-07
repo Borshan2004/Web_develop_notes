e0e4299cbdbb4ac76171efa4349ee706376559b2
Borshan2004 <borshanpurification42@gmail.com>
2025-10-07 21:20:30 +0600

    new

---
+++ module 19 loop in javascript/break.js
@@ -0,0 +1,28 @@
+//continue -> skip this one  iteration
+
+//break ->i am done with this loop and loop end 
+
+// for(let i=0;i<20;i++){
+//     console.log(i);
+//     if(i>=8){
+//         break;
+//     }
+// }
+
+
+
+for(let i=52;i>=0;i--){
+    if(i<40){
+        break;
+    }
+    console.log(i);
+}
+
+
+// for(let i=0;i<10;i++){
+//     if(i%2===1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
---
+++ module 19 loop in javascript/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+
+for(let i=52;i>=0;i--){
+    if(i<=40){
+        break;
+    }
+    console.log(i);
+}
