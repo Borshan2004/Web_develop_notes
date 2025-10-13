6a35eec1e21021aeea31652a6fecf4ed23b859c1
Borshan2004 <borshanpurification42@gmail.com>
2025-10-14 00:25:45 +0600

    add

---
+++ module 22 pratice of javascript problem/maximum_minimum.js
@@ -0,0 +1,27 @@
+const minimum = Math.min(45,50,32,12,51,56,33,2222,2,1202,-1);
+console.log(minimum);
+
+const maximum = Math.max(100,2,300,125,23,256,12302);
+console.log(maximum);
+
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-15));
+
+
+console.log(Math.round(4.2));
+console.log(Math.round(4.6));
+
+console.log(Math.floor(2.1));
+
+console.log(Math.ceil(2.1));
+
+
+console.log(Math.random());
+
+const n = Math.random()*10;
+console.log(n);
+
+const n2 = Math.round(Math.random()*10);
+console.log(n2);
