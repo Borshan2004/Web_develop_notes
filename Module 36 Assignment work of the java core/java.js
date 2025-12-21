76cf7bc0123e5810c1064c168203b592cbaffe5a
Borshan2004 <borshanpurification42@gmail.com>
2025-12-21 15:49:05 +0600

    add this new

--- Module 36 Assignment work of the java core/java.js
+++ Module 36 Assignment work of the java core/java.js
@@ -11,7 +11,7 @@ const work_with_buttons=(data_array)=>{
 for(num of data_array){
     const div = document.createElement("div");
     div.innerHTML=`
- <button class="btn btn-outline btn-primary"><img src="/B11-A6-English-Janala/assets/fa-book-open.png" onclick="buttons_click_work(${num.level_no})" alt="">Lesson-${num.level_no}</button>
+ <button class="btn btn-outline btn-primary" onclick="buttons_click_work(${num.level_no})"><img src="/B11-A6-English-Janala/assets/fa-book-open.png"  alt="">Lesson-${num.level_no}</button>
     `
     buttons_section.append(div);
 }
@@ -21,12 +21,44 @@ for(num of data_array){
 
 const buttons_click_work=(id)=>{
 
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(Response=>Response.json())
+    .then(data=>display_cards(data.data))
 
 
 }
 
-const display_cards=()=>{
+const display_cards=(data_word_array)=>{
 
+     const btnwork = document.getElementById("buttons_work");
+     btnwork.innerHTML = ""; 
+
+    for(eatch_data of data_word_array){
+
+        const div = document.createElement("div");
+
+        div.innerHTML=`
+    
+    <div class="card w-96 bg-base-100 card-xl shadow-sm">
+                <div class="card-body flex items-center">
+                    <h1 class="font-bold text-3xl">${eatch_data.word}</h1>
+                    <p class="text-base font-medium">Meaning/Pronounciation</p>
+                    <p class="text-xl font-semibold">${eatch_data.meaning}</p>
+                </div>
+                <div class="flex justify-between mx-6 mb-8">
+                    
+                    <img class="btn bg-white px-0.25 w-12 py-0.25" src="/B11-A6-English-Janala/assets/i_icon.png" alt="">
+                    <img class="btn bg-white px-0.25 w-12 py-0.25" src="/B11-A6-English-Janala/assets/sound_icon.png" alt="">
+                </div>
+    </div>
+    `
+
+    btnwork.append(div);
+
+    }
+    
+    
     
 }
 
