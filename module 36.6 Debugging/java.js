bfad805423e0f3326016f26461d3f388b93caa92
Borshan2004 <borshanpurification42@gmail.com>
2026-01-04 21:06:49 +0600

    add this

--- module 36.6 Debugging/java.js
+++ module 36.6 Debugging/java.js
@@ -1,11 +1,19 @@
 
-console.log("hello");
-// console.error("i am not okey");
+// console.log("hello");
+// // console.error("i am not okey");
 
-person={
-    name:"borshan",
-    age:13
+// person={
+//     name:"borshan",
+//     age:13
+// }
+
+// console.warn(person);
+// console.table(person);
+
+
+for(let i=0;i<10;i++){
+
+    const element = i;
+    console.log(element);
+
 }
-
-console.warn(person);
-console.table(person);
