27537154fe97d3403d254bf77699f45a1744e154
Borshan2004 <borshanpurification42@gmail.com>
2026-01-05 21:12:13 +0600

    add tihs nfads

---
+++ module 37 JavaScript You need to know for React/bracket_notaion.js
@@ -0,0 +1,13 @@
+const person={
+
+    name:"hasan",
+    friends:["rahim"],
+    10:"secret code",
+    
+}
+
+console.log(person.name);
+
+const herosecret = person[10];
+console.log(herosecret);
+
