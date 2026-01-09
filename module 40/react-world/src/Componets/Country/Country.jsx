c263daf222ed8ce7925f42fd4d862b381cca818d
Borshan2004 <borshanpurification42@gmail.com>
2026-01-10 00:36:34 +0600

    add this fdas

--- module 40/react-world/src/Componets/Country/Country.jsx
+++ module 40/react-world/src/Componets/Country/Country.jsx
@@ -7,17 +7,27 @@ const Country = ({api_pops}) => {
 
     const handle_work=()=>{
 
-        SetVisted(true)
+
+        if(visited==true){
+            SetVisted(false)
+        }
+        else{
+            SetVisted(true);
+        }
+
+        // SetVisted(!visited)
 
     }
 
 
     return (
+        //this is css for multiple class
         <div className={`box ${visited&& `box2`}`}>
             <p>Name of the country: {api_pops.name?.common}</p>
             <img className='center' src={api_pops.flags?.png} alt="pic of flag" />
             <p>Continent:{api_pops.continents}</p>
-            <button onClick={handle_work}>{visited?'Visited':'not visited'}</button>
+            {/* this css for single */}
+            <button className={visited?'box':'box2'} onClick={handle_work}>{visited?'Visited':'not visited'}</button>
         </div>
     );
 };
