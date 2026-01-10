b3efba082c75985b3a92ffef9f907a35c7e58fc2
Borshan2004 <borshanpurification42@gmail.com>
2026-01-10 16:16:59 +0600

    add this now please

--- module 40/react-world/src/Componets/Countries/Countries.jsx
+++ module 40/react-world/src/Componets/Countries/Countries.jsx
@@ -1,23 +1,65 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/country';
 import './Countries.css'
 
-const Countries = ({pops}) => {
+const Countries = ({ pops }) => {
 
     const api_work_Countries = use(pops);
+
+    const [visited_country, SetVisited_country] = useState([]);
+
+    const [called_flag, SetCalled_flag] = useState([]);
+
+
+
+
+
+
+
+    const handle_for_visisted_country = (api_pops) => {
+        //to store the data and make updateable we use the store_visited_country as like temp
+        const store_visited_country = [...visited_country, api_pops];
+        SetVisited_country(store_visited_country);
+    }
+
+
+    const handle_for_flag = (flag) => {
+
+        const store_called_flag = [...called_flag, flag];
+        SetCalled_flag(store_called_flag);
+     
+    }
 
 
     return (
         <div>
             <h2>Treveling countries: {api_work_Countries.length}</h2>
-            
+            <h2>Visited cournty number:{visited_country.length}</h2>
+            <ul>
+                {
+                    visited_country.map(visit => <li>{visit.name.common}</li>)
+                }
+
+            </ul>
+
+            <div>
+                {
+                    called_flag.map(flag_loader=><img src={flag_loader.png} alt="pic of flag"></img>)
+
+                }
+            </div>
+
             <div className='grid_work'>
                 {
-          api_work_Countries.map(country=><Country key={country.cca3} api_pops={country}></Country>)
+                    api_work_Countries.map(country => <Country
+                        visited_country_handler_pops={handle_for_visisted_country}
+                        handle_for_flag_props = {handle_for_flag}
+                        key={country.cca3}
+                        api_pops={country}></Country>)
 
-             }
+                }
             </div>
-            
+
         </div>
     );
 };
--- module 40/react-world/src/Componets/Country/Country.jsx
+++ module 40/react-world/src/Componets/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({api_pops}) => {
+const Country = ({api_pops,visited_country_handler_pops,handle_for_flag_props}) => {
 
     const [visited,SetVisted] =useState(false);
 
@@ -15,6 +15,7 @@ const Country = ({api_pops}) => {
             SetVisted(true);
         }
 
+        visited_country_handler_pops(api_pops);
         // SetVisted(!visited)
 
     }
@@ -28,6 +29,7 @@ const Country = ({api_pops}) => {
             <p>Continent:{api_pops.continents}</p>
             {/* this css for single */}
             <button className={visited?'box':'box2'} onClick={handle_work}>{visited?'Visited':'not visited'}</button>
+            <button onClick={()=>handle_for_flag_props(api_pops.flags)}>Flag call</button>
         </div>
     );
 };
