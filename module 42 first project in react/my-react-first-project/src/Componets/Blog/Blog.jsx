82f5b9cb2ef76c8f60253e2a56f951320670f6d3
Borshan2004 <borshanpurification42@gmail.com>
2026-01-16 00:07:25 +0600

    add this now please this time

--- module 42 first project in react/my-react-first-project/src/Componets/Blog/Blog.jsx
+++ module 42 first project in react/my-react-first-project/src/Componets/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
     return (
@@ -11,8 +12,18 @@ const Blog = ({ blog }) => {
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
+                    <div className="author_work flex items-center justify-center gap-10 mr-15">
+                        <p>{blog.author}</p>
+                        <img className='w-15 pr-6' src={blog.author_img} alt="" />
+                        <FaBookmark size={25} />
+                    </div>
                     <h2 className="card-title">{blog.title}</h2>
                     <p>A card compo body there are title and actions parts</p>
+                    <div className="list_skill flex gap-1/2">
+                        {
+                            blog.hashtags.map(skill=><p>{skill}</p>)
+                        }
+                    </div>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">mark as</button>
                     </div>
