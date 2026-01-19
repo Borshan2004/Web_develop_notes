a0f5a771227ffcd282dfff8d431dbd2f659f9e96
Borshan2004 <borshanpurification42@gmail.com>
2026-01-19 12:38:37 +0600

    add tihs

---
+++ Module 44/my-ract-tailwind-daisyuiwork/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Navbar2 from './Compunds/Navbar2/Navbar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar2></Navbar2>
+    </>
+  )
+}
+
+export default App
