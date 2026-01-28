7caf40b879413ca048c9aaddb119b242d36c31b7
Borshan2004 <borshanpurification42@gmail.com>
2026-01-28 21:15:25 +0600

    add this now please

---
+++ module 47/my-book-poka/src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { createBrowserRouter } from "react-router";
+import Roots from '../Pages/Roots/Roots';
+import Errorpage from '../Pages/Errorpage/Errorpage';
+import Home from '../Pages/Home/Home';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Roots,
+    errorElement:<Errorpage></Errorpage>,
+    children:[{
+        index:true,
+        path:"/",
+        Component:Home,
+    }]
+  },
+]);
