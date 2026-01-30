251db285ab9601ca378556df2abe07c4fdbeb5ef
Borshan2004 <borshanpurification42@gmail.com>
2026-01-30 23:02:01 +0600

    ad

--- module 47/my-book-poka/src/Pages/SingleBook/SingleBook.jsx
+++ module 47/my-book-poka/src/Pages/SingleBook/SingleBook.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const SingleBook = ({bookitemloader}) => {
+const SingleBook = ({ bookitemloader }) => {
     return (
         <div >
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                    <img className='h-96 w-64'
+            <div className="card bg-base-100 w-96 h-137.5 shadow-sm border-2 border-gray-100">
+                <figure className='bg-gray-100 m-8'>
+                    <img className='h-96 w-64 m-6'
                         src={bookitemloader.image}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Card Title
+                         {bookitemloader.bookName}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">{bookitemloader.category}</div>
                         <div className="badge badge-outline">Products</div>
                     </div>
                 </div>
